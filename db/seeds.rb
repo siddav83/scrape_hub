23d6f400d4a9ea5a5973cdc2d24f0d99315300ed
nicolas vassel <nicolas.vassel@gmail.com>
2021-03-01 16:46:25 +0000

    Boraugh seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Boraughs ex
+Boraughs.create(name: "City of London", area: "1,1197", median_income: "60,000", population: "11,000")
+
+Boraughs.create(name: "Camden", area: "8,401583", median_income: "37,300", population: "101,000")
+
+Boraughs.create(name: "Hackney", area: "7,3591071", median_income: "29,400", population: "112,000")
+
+# Companies
+
+Companies.create(id_boraughs: 1, name:"", address: "", year_trading: "", )
