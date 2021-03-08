ba246f082d869fbd188080439bded1bd5db85eae
DanielAwaritefe <awaritefe@gmail.com>
2021-03-08 17:59:21 +0000

    small change on border

--- app/models/borough.rb
+++ app/models/borough.rb
@@ -4,7 +4,5 @@ class Borough < ApplicationRecord
   scope :income_max, -> (amount) { where("gross_income <= ?", amount) }
   scope :income_min, -> (amount) { where("gross_income >= ?", amount) }
 
-  scope :employment_rate_min, -> (amount) { where("employment_rate >= ?", amount) }
-  scope :employment_rate_max, -> (amount) { where("employment_rate <= ?", amount) }
 
 end
