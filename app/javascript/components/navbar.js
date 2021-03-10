b7931cb3c85fc3a8f6dd04164dc847e20c827f91
alec wayman <aejw_edu@pm.me>
2021-03-10 10:22:14 +0000

    quick changes

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const topbar = document.querySelector('.top-bar');
   if (topbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 100) {
+      if (window.scrollY >= 100 || window.location.pathname === "/boroughs") {
         topbar.classList.add('top-bar-white');
       } else {
         topbar.classList.remove('top-bar-white');
