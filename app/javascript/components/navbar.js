9029f180f975bbee23f4a77b47f13b7cda24011a
alec wayman <aejw_edu@pm.me>
2021-03-10 12:13:48 +0000

    js changes

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,18 +1,35 @@
+// const initUpdateNavbarOnScroll = () => {
+//   const topbar = document.querySelector('.top-bar');
+//   if($('body').is('.PageType')) {
+//     topbar.classList.add('top-bar-white');
+//   } else {
+//     if (topbar) {
+//       window.addEventListener('scroll', () => {
+//         if (window.scrollY >= 100) {
+//           topbar.classList.add('top-bar-white');
+//         } else {
+//           topbar.classList.remove('top-bar-white');
+//         }
+//       });
+//     }
+//   }
+// }
+
 const initUpdateNavbarOnScroll = () => {
   const topbar = document.querySelector('.top-bar');
-  if($('body').is('.PageType')) {
-    topbar.classList.add('top-bar-white');
-  } else {
+
+  if($('body').is('.top-bar-transparent-ident')) {
+    topbar.classList.add('top-bar-transparent');
     if (topbar) {
       window.addEventListener('scroll', () => {
         if (window.scrollY >= 100) {
-          topbar.classList.add('top-bar-white');
+          topbar.classList.remove('top-bar-tranparent');
         } else {
-          topbar.classList.remove('top-bar-white');
+          // topbar.classList.add('top-bar-tranparent');
         }
       });
     }
   }
 }
-
 export { initUpdateNavbarOnScroll };
+
